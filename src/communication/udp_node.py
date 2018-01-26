115e1c0c8d5f30b039e2d91524bf605968c57c70
ngoldfarb <nagoldfarb@wpi.edu>
2018-01-26 14:36:04 -0500

    first commit

---
+++ src/communication/udp_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from beginner_tutorials.srv import *
+import rospy
+
+udp = None
+
+def handle_usp(req):
+    #print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    msg = udp.send_packet(req.board,req.id,req.downstream)
+    return UDPMessageResponse(msg)
+
+def udp_server():
+    rospy.init_node('udp_server')
+    s = rospy.Service('udp', UDPMessage, handle_udp)
+    udp = UDP(9876)
+    print "UDP ready"
+    rospy.spin()
+
+if __name__ == "__main__":
+	udp_server()
