db3ec88796272ea2490baf4f4f484e4aac6e0ffc
ngoldfarb <nagoldfarb@wpi.edu>
2018-02-05 18:04:44 -0500

    publishes a maze

--- src/games/maze/maze_node.py
+++ src/games/maze/maze_node.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 import rospy
+import maze_helper
+import mazeBank
+from geometry_msgs.msg import Path
+from nav_msgs.msg import OccupancyGrid
+
+def make_maze(maze):
+    maze_pub = rospy.Publisher('tmaze', OccupancyGrid, queue_size=1)
+
+    row = len(maze)
+    col = len(maze[0])
+    data = [j for i in maze for j in i]
+    my_maze = OccupancyGrid()
+
+    my_maze.data = data
+    my_maze.info.width = row
+    my_maze.info.height = col
+    my_maze.header.stamp = rospy.Time.now()
 
 
-def start(map):
+
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("usage: my_node.py arg1 arg2")
-    else:
-        start(sys.argv[0])
+    rospy.init_node("maze")
+    maze_name = sys[0]
+    my_maze = mazeBank(make_name)
+    make_maze(maze)
+
+    while not rospy.is_shutdown():
+        pass
